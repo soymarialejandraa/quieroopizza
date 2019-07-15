acb4dfd8c01856bd06712f77b5998bcd17796a87
soymarialejandraa <alejandracaballero95@hotmail.com>
2019-07-15 15:01:38 -0600

    Agregamos una lista de productos

---
+++ QuierooPizza/QuierooPizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+﻿using QuierooPizza.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuierooPizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Pizza 6 quesos";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Pizza 4 Estaciones";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Pizza Jamon y Queso";
+
+            var listadeProductos = new List<ProductoModel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return View(listadeProductos);
+        }
+    }
+}
---
+++ QuierooPizza/QuierooPizza.Web/Models/ProductoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuierooPizza.Web.Models
+{
+    public class ProductoModel
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+
+}
